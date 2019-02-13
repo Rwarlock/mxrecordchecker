87f0c312f830ad205575bbeb57c244daacf9c336
Rwarlock <bhanukrsingh@gmail.com>
2019-02-13 18:31:08 +0530

    First push

---
+++ mxrecord.py
@@ -0,0 +1,68 @@
+import csv
+import re
+import dns.resolver
+import socket
+import smtplib
+
+input_file = open("test_list.csv", "r").readlines()
+with open("Final_file.csv","w") as contact:
+	employee_writer = csv.writer(contact, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	print(len(input_file))
+	csv_reader = csv.reader(input_file)
+	regex = re.compile(
+	    r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$')
+	try:
+	    for line_count, row in enumerate(csv_reader, 1):
+	        print('Checking {0} of {1}'.format(line_count, len(input_file)))
+	        # Don't make a set out of this
+	        fname = row[0]
+	        lname = row[1]
+	        # Don't make a list out of this; trim spaces
+	        email = row[2].strip()
+	        print(email)
+	        print('Checking contact name {}'.format(fname))
+	        match = regex.match(email)
+	        if match is None:
+	            print("Bad Email")
+	            employee_writer.writerow([fname,lname,email,'Invalid Email'])
+	        else:
+        	# try:
+        		# Spliting the mail ID to get uper level TLDs
+	        	splitAddress = email.split('@')
+	        	domain = str(splitAddress[1])
+	        	records = dns.resolver.query(domain, 'MX')
+	        	mxRecord = records[0].exchange
+	        	mxRecord = str(mxRecord)
+	        	if 1==1:
+	        		server = smtplib.SMTP()
+				server.set_debuglevel(0)
+				# SMTP Conversation
+				server.connect(mxRecord)
+				server.helo(server.local_hostname) 
+				### server.local_hostname(Get local server hostname)
+				server.mail('corn@bt.com')
+				code, message = server.rcpt(str(email))
+				server.quit()
+				if code == 250:
+					print('Success smpt mail')
+					employee_writer.writerow([fname,lname,email,'Valid Email'])
+				else:
+					print('Bad smpt mail')
+		        print("Good Email") 
+		        print(mxRecord)
+		        # SMTP lib setup (use debug level for full output)
+		        	
+	        	print('')
+	        	print('')
+	        	print('')
+	        	print('')
+        	# except:
+        		print("Bad Domain")
+        		employee_writer.writerow([fname,lname,email,'Invalid Domain'])
+	        	
+	      
+	except IndexError as error:
+		print('')
+		print('')
+		print('')
+		print('###################################')
